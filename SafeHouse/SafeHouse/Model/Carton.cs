87612c387f3359d67ebe739fd44620112c1b7cd0
Nikola Zivkovic <zivkovic.nikola87@gmail.com>
2018-09-07 18:18:13 +0200

    Project setup

---
+++ SafeHouse/SafeHouse/Model/Carton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Svratiste.Model
+{
+    public class Carton
+    {
+        public Guid Id { get; set; }
+        public String FirstName { get; set; }
+        public String MiddleName { get; set; }
+        public String LastName { get; set; }
+        public String CartonNumber { get; set; }
+        public int Gender { get; set; }
+        public int Age { get; set; }
+        public int NumberOfVisits { get; set; }
+    }
+}
---
+++ SafeHouse/SafeHouse/Model/Evaluation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Svratiste.Model
+{
+    public class Evaluation
+    {
+        public Guid Id { get; set; }
+        public Carton Carton { get; set; }
+        public String AddressStreetName { get; set; }
+        public String AddressStreetNumber { get; set; }
+
+        // TODO: Add other fields
+    }
+}
