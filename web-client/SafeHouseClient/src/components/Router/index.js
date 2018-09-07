930d7805e8198605b326d7da2d3431884ce090d6
b.bjekovic <b.bjekovic@vegaitsourcing.rs>
2018-09-07 22:20:56 +0200

    Login component

--- web-client/SafeHouseClient/src/components/Router/index.js
+++ web-client/SafeHouseClient/src/components/Router/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Cardboard from "../../views/Cardboard";
+import Login from "../../views/Login";
 
 const AppRouter = () => {
     return (
         <Switch>
-            {/* Sections */}
+            {/* Cardboard */}
             <Route exact path="/" component={Cardboard} />
+
+            {/* Login */}
+            <Route exact path="/login" component={Login} />
         </Switch>
     );
 };
---
+++ web-client/SafeHouseClient/src/views/Login.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <h2>Login</h2>
+        );
+    };
+}
+
+export default Login;
