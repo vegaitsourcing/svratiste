9be1a9866ef0464fb53e107f42ccf362cb7a1570
Nikola Zivkovic <zivkovic.nikola87@gmail.com>
2018-09-07 23:25:40 +0200

    New constrains Fixups

--- web-api/SafeHouse.Data/Entities/Suitability.cs
+++ web-api/SafeHouse.Data/Entities/Suitability.cs
@@ -1,9 +1,11 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SafeHouse.Data.Entities 
 {
     public class Suitability : BaseEntity
     {
+        [MaxLength(1024)]
         public string Description { get; set; }
     }
 }
