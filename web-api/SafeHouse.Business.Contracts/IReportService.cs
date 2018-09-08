170e0dcffbc54b7cba01ab105bd920695ff2ef27
Nikola Zivkovic <zivkovic.nikola87@gmail.com>
2018-09-08 10:33:00 +0200

    Report Service

--- web-api/SafeHouse.Business.Contracts/IReportService.cs
+++ web-api/SafeHouse.Business.Contracts/IReportService.cs
@@ -1,6 +1,10 @@
-﻿namespace SafeHouse.Business.Contracts
+﻿using SafeHouse.Business.Contracts.Models;
+using System;
+
+namespace SafeHouse.Business.Contracts
 {
     public interface IReportService
     {
+        ReportDataDto GetReportData(DateTime start, DateTime end, Guid visitor);
     }
 }
--- web-api/SafeHouse.Business/ReportService.cs
+++ web-api/SafeHouse.Business/ReportService.cs
@@ -1,8 +1,21 @@
-﻿using SafeHouse.Business.Contracts;
+﻿using System;
+using SafeHouse.Business.Contracts;
+using SafeHouse.Business.Contracts.Models;
 
 namespace SafeHouse.Business
 {
     public class ReportService : IReportService
     {
+        private readonly SafeHouseContext _dbContex;
+
+        public ReportService(SafeHouseContext context)
+        {
+            _dbContex = context;
+        }
+
+        public ReportDataDto GetReportData(DateTime start, DateTime end, Guid visitor)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
