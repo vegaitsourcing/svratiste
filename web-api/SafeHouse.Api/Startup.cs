6bbb2f7207260f70bbaec159e24bf89e844e8293
Ognjen Stanic <o.stanic@vegaitsourcing.rs>
2018-09-08 20:48:34 +0200

    Created SPA by including web-client on web-api

--- web-api/SafeHouse.Api/Startup.cs
+++ web-api/SafeHouse.Api/Startup.cs
@@ -1,14 +1,10 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
-using SafeHouse.Api.Helpers;
 using SafeHouse.Data;
-using SafeHouse.Data.Entities;
 using SafeHouse.Infrastructure;
 
 namespace SafeHouse.Api
@@ -28,37 +24,16 @@ namespace SafeHouse.Api
             var connection = Configuration.GetConnectionString("DefaultConnection");
 
             services.AddDataServices(connection)
-                .AddBusinessServices()
-                .AddMvc();
+                .AddBusinessServices();
 
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-
-                    ValidIssuer = Configuration.GetValue<string>(Api.Common.Constants.ConfigKeys.Issuer),
-                    ValidAudience = Configuration.GetValue<string>(Api.Common.Constants.ConfigKeys.Audience),
-                    IssuerSigningKey = JwtSecurityKey.Create(Configuration.GetValue<string>(Api.Common.Constants.ConfigKeys.Secreet))
-                    };
-
-                    options.Events = new JwtBearerEvents
-                    {
-                        OnAuthenticationFailed = context => Task.CompletedTask,
-                        OnTokenValidated = context => Task.CompletedTask
-                    };
-                });
-
-            services.AddAuthorization(options =>
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
             {
-                options.AddPolicy("Member",
-                    policy => policy.RequireClaim(Api.Common.Constants.SafeHouseUserIdClaimKey));
+                configuration.RootPath = "../../web-client/SafeHouseClient/build";
             });
 
+            services.AddAuthorizationServices(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -68,9 +43,34 @@ namespace SafeHouse.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "../../web-client/SafeHouseClient";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
+            });
 
             app.UseAuthentication();
-            app.UseMvc();
 
             db.EnsureSeedData();
         }
