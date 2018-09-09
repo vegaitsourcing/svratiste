53a0584204917522316439f12c8ccd1171f92b52
b.bjekovic <b.bjekovic@vegaitsourcing.rs>
2018-09-09 11:50:22 +0200

    Added token to FE

--- web-client/SafeHouseClient/src/actions/LoginActions.js
+++ web-client/SafeHouseClient/src/actions/LoginActions.js
@@ -5,7 +5,6 @@ import { web_api_url } from '../constants';
 
 export function login(credentials) {
     axios.post(web_api_url + '/token', credentials).then((response) => {
-        console.log(response.data);
         dispatcher.dispatch({
             type: "FETCHED_TOKEN",
             payload: response.data
---
+++ web-client/SafeHouseClient/src/authToken.js
@@ -0,0 +1,7 @@
+
+
+export function getToken() {
+    var token = localStorage.getItem('accessToken');
+
+    return token;
+} 
