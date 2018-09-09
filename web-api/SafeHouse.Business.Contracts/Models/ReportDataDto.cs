0732de33df683877cc696a26dfd28f8057b56d87
Nikola Zivkovic <zivkovic.nikola87@gmail.com>
2018-09-09 16:58:42 +0200

    Report Data modifications Notifications implementataions

--- web-api/SafeHouse.Business.Contracts/Models/ReportDataDto.cs
+++ web-api/SafeHouse.Business.Contracts/Models/ReportDataDto.cs
@@ -3,19 +3,12 @@
     public class ReportDataDto
     {
         public int GuestsCount { get; set; }
+        public int FemaleGuestsCount { get; set; }
+        public int MaleGuestsCount { get; set; }
         public int VisitsCount { get; set; }
-        public int BrekfastCount { get; set; }
-        public int LunchCount { get; set; }
-        public int DinnerCount { get; set; }
+        public int MealCount { get; set; }
         public int BathsCount { get; set; }
         public int LiecesRemovedCount { get; set; }
         public int ClothingSumCount { get; set; }
-        public int MediationWritingCount { get; set; }
-        public int MediationOralCount { get; set; }
-        public int EducationCount { get; set; }
-        public int PsihoSocialSupportCount { get; set; }
-        public int SchoolSupportCount { get; set; }
-        public int ContactWithParentsCount { get; set; }
-        public int MedicalInterventionCount { get; set; }
     }
 }
