eb01424e7ef6f6a507e8e032c740b1333ec36e0b
Luka Vuckovic <l.vuckovic@vegaitsourcing.rs>
2019-09-15 20:26:57 +0200

    Fix Auth

--- app/SafeHouse.Core/UseCases/AccountService.cs
+++ app/SafeHouse.Core/UseCases/AccountService.cs
@@ -21,11 +21,17 @@ namespace SafeHouse.Core.UseCases
 
         public Guid? GetUserIdForCredentials(CheckCredentialsRequest request)
         {
-            return _userRepository.GetBy(user => user.Username == request.Username)
-                .Where(user => IsValid(request.Password, user.Password))
-                .DefaultIfEmpty(new SafeHouseUser())
-                .First()
-                .Id;
+            if(request.Username == "safehouse" && request.Password == "S@feH0use20!8")
+            {
+                return _userRepository.GetBy(user => user.Username == request.Username)
+                    .Where(user => IsValid(request.Password, user.Password))
+                    .DefaultIfEmpty(new SafeHouseUser())
+                    .First()
+                    .Id;
+            } else
+            {
+                return null;
+            }
         }
 
         private bool IsValid(string password, string hashedPassword)
